f262ce6a9f904af67bc98e9d3bbd2d5f608634a9
onejuliancode <jcorrea@cargocorpuw.com>
2024-01-30 11:57:54 -0500

    :sparkles: Agrega un test unitario de jwt

---
+++ tests/jwt_test/jw_test.go
@@ -0,0 +1,15 @@
+package jwt_test
+
+import (
+	"favorite-characters/src/infraestructure/jwt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateJWT(t *testing.T) {
+	assert := assert.New(t)
+
+	password_encoded := jwt.HashAndSalt([]byte("holamundo"))
+	assert.NotEmpty(password_encoded)
+}
